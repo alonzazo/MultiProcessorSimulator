7cf3ed08f680736e6c24eb0241a7acd0dfa5da21
Francisco Arroyo <fran.arroyo596@gmail.com>
2017-11-06 10:15:06 -0600

    Agregadas estructuras de datos

---
+++ MultiProcessorSimulator/MultiProcessorSimulator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiProcessorSimulator
+{
+    class Program
+    {
+        private MemCompartida memCompartida;
+        private MemInstrucciones memInstrucciones;
+
+        private CacheDatos cacheDatosN0;
+        private CacheDatos cacheDatosN1;
+        private CacheDatos cacheDatosN2;
+
+        private CacheInstrucciones cacheInstruccionesN1;
+        private CacheInstrucciones cacheInstruccionesN2;
+        private CacheInstrucciones cacheInstruccionesN3;
+
+        private Directorios directorios;
+        private Contexto contextoP0;
+        private Contexto contextoP1;
+
+        private Registros registrosN0;
+        private Registros registrosN1;
+        private Registros registrosN2;
+        private RegistrosInstruccion rInstruccionN0;
+        private RegistrosInstruccion rInstruccionN1;
+        private RegistrosInstruccion rInstruccionN2;
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ MultiProcessorSimulator/MultiProcessorSimulator/RegistrosInstruccion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiProcessorSimulator
+{
+    class RegistrosInstruccion
+    {
+        public int[] IR;
+
+        public RegistrosInstruccion()
+        {
+            IR = new int[4];
+        }
+    }
+}
